9f80a7defbd943efee98afb6dc38b9799c935dbc
Josh Sun <joshuasun1998@icloud.com>
2017-05-08 18:51:24 -0400

    formatted I/O

---
+++ Parser.hpp
@@ -0,0 +1,20 @@
+//
+//  Parser.hpp
+//  Scheme_Interpreter
+//
+//  Created by Josh Sun on 2017-05-08.
+//  Copyright © 2017 Josh Sun. All rights reserved.
+//
+
+#ifndef Parser_hpp
+#define Parser_hpp
+
+#
+
+class parser {
+public:
+private:
+protected:
+};
+
+#endif /* Parser_hpp */
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Scheme_Interpreter
+//
+//  Created by Josh Sun on 2017-05-08.
+//  Copyright © 2017 Josh Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
